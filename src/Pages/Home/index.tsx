8b650a58f828ce827f05eef38dea590232a007a8
LuanAnders0n <luan.anderson.001@gmail.com>
2023-10-25 13:51:38 -0300

    Card

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Container } from './styled';
+import perfil from '../../images/perfil.jpeg';
+import GitHub from '../../images/github.svg';
+import Linkedin from '../../images/3225190_app_linkedin_logo_media_popular_icon.svg';
+import Instagram from '../../images/1217086_instagram_icon.svg';
+
+const Home = () => {
+  return (
+    <div>
+      <Container>
+        <div className="img-card">
+          <img className="img" src={perfil} alt="" />
+        </div>
+        <a href="https://www.linkedin.com/in/luananders0n/">
+          <button className="card">
+            <img className="icons" src={Linkedin} alt="" />
+            <h1>LINKEDIN</h1>
+          </button>
+        </a>
+        <a href="https://github.com/LuanAnders0n">
+          <button className="card">
+            <img className="icons" src={GitHub} alt="" />
+            <h1>GITHUB</h1>
+          </button>
+        </a>
+        <a href="https://www.instagram.com/luan_white/">
+          <button className="card">
+            <img className="icons" src={Instagram} alt="" />
+            <h1>INSTAGRAM</h1>
+          </button>
+        </a>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Home/styled.ts
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import background from '../../images/background.jpg';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${background});
+  background-size: cover;
+
+  .img-card {
+    margin: 20px;
+    width: 300px;
+    height: 300px;
+    border-radius: 100%;
+    background-color: #171f31;
+    background: #c4c4c5;
+    background: linear-gradient(145deg, #d2d2d3, #b0b0b1);
+    box-shadow: inset 8px 8px 16px #fff, inset -8px -8px 16px #6a6a6a;
+  }
+
+  .img {
+    width: 300px;
+    height: 300px;
+    border-radius: 100%;
+    border: 3px solid #fff;
+  }
+
+  .icons {
+    width: 100px;
+    height: 100px;
+    float: left;
+  }
+
+  .card {
+    margin: 20px;
+    width: 750px;
+    height: 190px;
+    background-color: #171f31;
+    border-radius: 29px;
+    background: #1c2435;
+    box-shadow: inset -8px -8px 16px #0f131d, inset 8px 8px 16px #29354d;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    color: #fff;
+    font-weight: 900;
+  }
+
+  a {
+    text-decoration: none;
+    color: #fff;
+  }
+
+  @media only screen and (max-width: 1200px) {
+  }
+`;
