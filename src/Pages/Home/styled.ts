f1a6244e35edd1f0b1c0e57c272f1031c655df33
LuanAnders0n <luan.anderson.001@gmail.com>
2023-10-25 14:07:25 -0300

    Style

--- src/Pages/Home/styled.ts
+++ src/Pages/Home/styled.ts
@@ -2,11 +2,14 @@ import styled from 'styled-components';
 import background from '../../images/background.jpg';
 
 export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   .back {
     background-image: url(${background});
-    width: 100%;
-    height: 100%;
-    z-index: -1;
+    width: 100vw;
+    height: 100vh;
     display: flex;
     align-items: center;
     justify-content: center;
